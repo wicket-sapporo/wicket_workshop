65818debdbd00c19ae4be24db1f994c24be8e834
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2013-08-29 19:55:08 +0900

    - wicket:childタグを使ったテンプレートのサンプルがほぼ完成

--- src/main/java/org/wicket_sapporo/workshop01/page/template/childtag_pattern/HeaderAndFooterPage.java
+++ src/main/java/org/wicket_sapporo/workshop01/page/template/childtag_pattern/HeaderAndFooterPage.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.wicket_sapporo.workshop01.page.template.childtag_pattern;
 
 import org.apache.wicket.bootstrap.Bootstrap;
@@ -6,9 +22,17 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.Model;
 
+/**
+ * ヘッダおよびフッタをテンプレートとして表示するページ.
+ *
+ * @author Hiroto Yamakawa
+ */
 public class HeaderAndFooterPage extends WebPage {
 	private static final long serialVersionUID = 4581732758124803759L;
 
+	/**
+	 * Construct.
+	 */
 	public HeaderAndFooterPage() {
 		add(new Label("headerLabel", Model.of("ヘッダ部分です")));
 		add(new Label("fotterLabel", Model.of("フッタ部分です")));
@@ -18,8 +42,8 @@ public class HeaderAndFooterPage extends WebPage {
 	public void renderHead(IHeaderResponse response) {
 		super.renderHead(response);
 
-		//wicket-bootstrap.jar を含んだプロジェクトでは、以下の様に書くと、Bootsrapを使うためのhtmlヘッダが書き込まれます。
-		//Wicket v6.10.0用のwicket-bootstrap v0.12では、Bootstrapv2.3.2 が利用できます。
+		// wicket-bootstrap.jar を導入したプロジェクトで、以下の様に書くと、Bootsrapを使うためのhtmlヘッダが書き込まれます。
+		// Wicket v6.10.0用のwicket-bootstrap v0.12では、Bootstrapv2.3.2 が利用できます。
 		Bootstrap.renderHeadPlain(response);
 		Bootstrap.renderHeadResponsive(response);
 	}
--- src/main/java/org/wicket_sapporo/workshop01/page/template/replace_pattern/BasePage.java
+++ src/main/java/org/wicket_sapporo/workshop01/page/template/replace_pattern/BasePage.java
@@ -1,7 +1,17 @@
 package org.wicket_sapporo.workshop01.page.template.replace_pattern;
 
+import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.WebPage;
 
 public class BasePage extends WebPage {
+
+	Component header;
+	Component footer;
+	Component menu;
+	Component content;
+
+	public BasePage() {
+
+	}
 
 }
