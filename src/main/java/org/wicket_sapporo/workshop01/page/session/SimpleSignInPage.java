454923114fe0719a422fb36348acae3dc41f1fd1
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2013-10-13 03:19:50 +0900

    Cosmetic change.

--- src/main/java/org/wicket_sapporo/workshop01/page/session/SimpleSignInPage.java
+++ src/main/java/org/wicket_sapporo/workshop01/page/session/SimpleSignInPage.java
@@ -3,9 +3,9 @@
  * file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file
  * to You under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
  * License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
@@ -24,9 +24,9 @@ import org.wicket_sapporo.workshop01.page.WS01TemplatePage;
 
 /**
  * サインイン画面の体で.
- * 
+ *
  * @author Hiroto Yamakawa
- * 
+ *
  */
 public class SimpleSignInPage extends WS01TemplatePage {
 	private static final long serialVersionUID = -8371810037545557093L;
@@ -49,7 +49,7 @@ public class SimpleSignInPage extends WS01TemplatePage {
 			@Override
 			protected void onSubmit() {
 				super.onSubmit();
-				if ((WS01Session.get()).signIn(userId, passphrase)) {
+				if (WS01Session.get().signIn(userId, passphrase)) {
 					setResponsePage(SignedPage.class);
 				}
 				// 失敗したら FeedBackPanel がエラーメッセージを表示する様に、メッセージをセット.
