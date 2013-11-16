c3c0bd10d2e80a2ea276b55d4394632fb218c37d
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2013-11-16 11:51:16 +0900

    Start programming of the unit test code.

--- src/test/java/org/wicket_sapporo/workshop01/page/WS01IndexPageTest.java
+++ src/test/java/org/wicket_sapporo/workshop01/page/WS01IndexPageTest.java
@@ -3,14 +3,20 @@
  */
 package org.wicket_sapporo.workshop01.page;
 
+import java.util.Locale;
+
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
 import org.junit.Test;
 import org.wicket_sapporo.workshop01.WS01Application;
+import org.wicket_sapporo.workshop01.WS01Session;
+import org.wicket_sapporo.workshop01.page.basics.SimplePage;
+import org.wicket_sapporo.workshop01.page.form.FormPage;
 
 /**
- * @author CIST yamakawa
+ * WS01IndexPageのテストクラス
  *
+ * @author Hiroto Yamakawa
  */
 public class WS01IndexPageTest {
 	private WicketTester tester;
@@ -18,12 +24,45 @@ public class WS01IndexPageTest {
 	@Before
 	public void setUp() {
 		tester = new WicketTester(new WS01Application());
+		// Localeを日本語に設定（
+		WS01Session session = (WS01Session) tester.getSession();
+		session.setLocale(Locale.JAPANESE);
 	}
 
 	@Test
 	public void 初期状態でホームページが表示される() {
-		tester.startPage(WS01IndexPage.class);
+		// テストとページの表示のスタート
+		tester.startPage(new WS01IndexPage());
+		// 最後に表示されたページのassert
 		tester.assertRenderedPage(WS01IndexPage.class);
+		tester.assertLabel("helloMessage", "こんにちは！ ここから先のページはWicketを使って表示されています！");
+	}
+
+	@Test
+	public void SimplePageに移動できる() {
+		// テストとページの表示のスタート
+		tester.startPage(new WS01IndexPage());
+		tester.clickLink("toSimplePage", false);
+		tester.assertRenderedPage(SimplePage.class);
+	}
+
+	@Test
+	public void FormPageに移動できる() {
+		// テストとページの表示のスタート
+		tester.startPage(new WS01IndexPage());
+		tester.clickLink("toFormPage", false);
+		tester.assertRenderedPage(FormPage.class);
+	}
+
+
+	@Test
+	public void 英語環境で初期状態でホームページが表示される() {
+		// テストとページの表示のスタート
+		tester.startPage(new WS01IndexPage());
+		WS01Session session = (WS01Session) tester.getSession();
+		session.setLocale(Locale.US);
+		tester.assertRenderedPage(WS01IndexPage.class);
+		tester.assertLabel("helloMessage", "Hello! These pages have been displayed using Wicket!");
 	}
 
 }
---
+++ src/test/java/org/wicket_sapporo/workshop01/page/form/FormPageTest.java
@@ -0,0 +1,41 @@
+package org.wicket_sapporo.workshop01.page.form;
+
+import java.util.Locale;
+
+import org.apache.wicket.util.tester.FormTester;
+import org.apache.wicket.util.tester.WicketTester;
+import org.junit.Before;
+import org.junit.Test;
+import org.wicket_sapporo.workshop01.WS01Application;
+import org.wicket_sapporo.workshop01.WS01Session;
+
+public class FormPageTest {
+
+	private WicketTester tester;
+
+	@Before
+	public void setUp() {
+		tester = new WicketTester(new WS01Application());
+		WS01Session session = (WS01Session) tester.getSession();
+		session.setLocale(Locale.JAPANESE);
+	}
+
+	@Test
+	public void ページが表示される() {
+		// テストとページの表示のスタート
+		tester.startPage(new FormPage());
+		// 最後に表示されたページクラスのassert
+		tester.assertRenderedPage(FormPage.class);
+	}
+
+	@Test
+	public void FormをsubmitするとFormConfirmationPageが表示される() {
+		// テストとページの表示のスタート
+		tester.startPage(new FormPage());
+		FormTester formTester = tester.newFormTester("form", false);
+		formTester.setValue("name", "foo");
+		formTester.submit();
+		tester.assertRenderedPage(FormConfirmationPage.class);
+	}
+
+}
