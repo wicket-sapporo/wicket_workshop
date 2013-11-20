2f0a13ad7bc7790198e65224e24c6b3106e8059e
CIST yamakawa <yamakawa@photon.chitose.ac.jp>
2013-11-20 14:50:23 +0900

    Adjust comments.

--- src/main/java/org/wicket_sapporo/springApp/SpringAppSession.java
+++ src/main/java/org/wicket_sapporo/springApp/SpringAppSession.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.wicket_sapporo.springApp;
 
 import org.apache.wicket.Session;
@@ -8,7 +24,7 @@ import org.apache.wicket.util.lang.Args;
 /**
  * SpringApp用のWebSessionクラス.
  *
- * @author CIST yamakawa
+ * @author Hiroto Yamakawa
  */
 public class SpringAppSession extends WebSession {
 	private static final long serialVersionUID = -7905878838938476571L;
